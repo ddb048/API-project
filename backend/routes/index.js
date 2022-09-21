6a0ebfbeb7e2cb86e7aa5a7f3ad61d898517eb5e
david <ddb048@gmail.com>
2022-09-21 12:54:38 -0700

    phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -9,5 +10,8 @@ router.get("/api/csrf/restore", (req, res) => {
     });
 });
 // ..
+
+
+router.use('/api', apiRouter);
 
 module.exports = router;
