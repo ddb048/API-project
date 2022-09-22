01ffc144703d35f95899116c3ce7a40f72013b4f
david <ddb048@gmail.com>
2022-09-22 15:28:54 -0700

    user middlewares added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const { restoreUser } = require("../../utils/auth.js");
+router.use(restoreUser);
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+
+
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
+
+
 
 module.exports = router;
