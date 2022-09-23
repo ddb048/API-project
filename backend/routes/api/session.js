1df41fee22cedad5d7127236a4e649492677c1f4
david <ddb048@gmail.com>
2022-09-23 10:40:05 -0700

    added firstName and lastName to model and migration as well as endpoints

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,11 +2,27 @@ const express = require('express')
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
+// Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -25,6 +41,20 @@ router.post(
         return res.json({
             user
         });
+    }
+);
+
+
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
     }
 );
 
