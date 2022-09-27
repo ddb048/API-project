696a66637981c92ea8519addf58d08a867ab57b6
david <ddb048@gmail.com>
2022-09-26 20:31:33 -0700

    seeders/models/migrations for group and user

---
+++ backend/db/models/venue.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Venue extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Venue.init({
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.DECIMAL(9, 7),
+      allowNull: false
+    },
+    lng: {
+      type: DataTypes.DECIMAL(10, 7),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Venue',
+  });
+  return Venue;
+};
