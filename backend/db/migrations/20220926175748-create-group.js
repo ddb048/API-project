68c5e9a73038d6c0479cc6bcfd7f764b5ede28a5
david <ddb048@gmail.com>
2022-09-26 17:10:47 -0700

    seeder created groups

---
+++ backend/db/migrations/20220926175748-create-group.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      organizerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Users", key: "id" }
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      about: {
+        type: Sequelize.BLOB,
+        allowNull: false
+      },
+      type: {
+        type: Sequelize.ENUM({
+          values: ["In person", "Online"]
+        }),
+        allowNull: false
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Groups');
+  }
+};
