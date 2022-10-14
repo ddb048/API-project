f4bcc9e3fa4d2d58b9f52886aa85ec3057f64763
david <ddb048@gmail.com>
2022-10-14 13:16:10 -0700

    thunks for login completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,22 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import * as sessionActions from './store/session';
 
 import configureStore from './store';
 
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+
 const store = configureStore();
 
+
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
---
+++ frontend/src/store/session.js
@@ -0,0 +1,54 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+//action creator to set a User
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+//action creator to remove the User
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    //uses the csrfFetch function to ensure proper auth
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data));
+    return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
