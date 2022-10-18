904addcc14d3f89568d03127f5b80f46891c5b1e
david <ddb048@gmail.com>
2022-10-18 15:09:33 -0700

    group reducer created

---
+++ frontend/src/store/groups.js
@@ -0,0 +1,43 @@
+import { csrfFetch } from "./csrf";
+
+//TYPES
+const LOAD_GROUPS = 'groups/LOAD'
+
+//ACTION CREATOR
+const loadGroups = groups => ({
+    type: LOAD_GROUPS,
+    groups
+})
+
+//THUNKS
+export const getAllGroups = () => async dispatch => {
+    const response = await fetch('/api/groups');
+
+    if (response.ok) {
+        const groups = await response.json();
+        console.log('groups ', groups);
+        dispatch(loadGroups(groups));
+        return groups;
+    }
+}
+
+//REDUCER
+const initialState = {
+    allGroups: {},
+    Group: {
+        groupImages: [],
+        organizer: {},
+        venues: []
+    }
+}
+
+export const groupReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_GROUPS:
+            action.groups.reduce((accum, curr) => {
+                accum[curr.id] = curr;
+                return accum;
+            }, allGroups);
+            return { ...state, allGroups }
+    }
+}
