4dc08041083838e46367804992a711af698eb053
david <ddb048@gmail.com>
2022-10-18 23:52:24 -0700

    finished thunks for Groups

--- frontend/src/store/groups.js
+++ frontend/src/store/groups.js
@@ -1,49 +1,114 @@
 import { csrfFetch } from "./csrf";
 
-//*******************TYPES*********************
+//*******************TYPES*********************/
 const LOAD_GROUPS = 'groups/LOAD'
 const ADD_GROUP = 'groups/ADD'
 const REMOVE_GROUP = 'groups/REMOVE'
 const EDIT_GROUP = 'groups/EDIT'
 const LOAD_ONE_GROUP = 'groups/LOAD_ONE'
 
-//****************ACTION CREATOR***************
+//****************ACTION CREATOR***************/
 const loadGroups = groups => ({
     type: LOAD_GROUPS,
     groups
 })
 
-const addGroup = newGroup => {
+const addGroup = newGroup => ({
     type: ADD_GROUP,
-        newGroup
-}
-/********************THUNKS*********************
+    newGroup
+})
+
+const removeGroup = groupId => ({
+    type: REMOVE_GROUP,
+    groupId
+})
+
+const editGroup = group => ({
+    type: EDIT_GROUP,
+    group
+})
+
+const loadOneGroup = group => ({
+    type: LOAD_ONE_GROUP,
+    group
+})
+
+/********************THUNKS*********************/
+//GET /api/groups (READ)
 export const getAllGroups = () => async dispatch => {
-    const response = await fetch('/api/groups');
+    const response = await csrfFetch('/api/groups');
 
     if (response.ok) {
         const groups = await response.json();
         console.log('groups ', groups);
         dispatch(loadGroups(groups));
+        return groups;
     }
-    return groups;
-}
 
-//*******************REDUCER********************
+}
+//POST /api/groups (CREATE)
+export const createGroup = (newGroup) => async dispatch => {
+    const response = await csrfFetch('/api/groups', {
+        method: 'POST',
+        body: JSON.stringify(newGroup)
+    });
+
+    if (response.ok) {
+        const newGroup = await response.json();
+        dispatch(addGroup(newGroup));
+        return newGroup;
+    }
+}
+//DELETE /api/groups/:groupId (DELETE)
+export const deleteGroup = (groupId) => async dispatch => {
+    const response = await csrfFetch(`/api/groups/${groupId}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        const group = await response.json();
+        dispatch(removeGroup(group));
+        return group;
+    }
+}
+//PUT /api/groups/:groupId (UPDATE)
+export const updateGroup = group => async dispatch => {
+    const response = await csrfFetch(`/api/groups/${group.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(group)
+    });
+
+    if (response.ok) {
+        const group = await response.json();
+        dispatch(editGroup(group));
+        return group;
+    }
+}
+//GET /api/groups/:groupId (READ1)
+export const getGroupDetails = groupId => async dispatch => {
+    const response = await csrfFetch(`/api/groups/${groupId}`);
+
+    if (response.ok) {
+        const group = await response.json();
+        dispatch(loadOneGroup(group));
+        return group;
+    }
+}
+/*******************REDUCER********************/
 const initialState = {
     groups: {}
 }
 
 
 export const groupReducer = (state = initialState, action) => {
+    let newState;
     switch (action.type) {
         case LOAD_GROUPS:
-            const allGroups = {};
-            action.groups.reduce((accum, curr) => {
-                accum[curr.id] = curr;
-                return accum;
-            }, allGroups);
-            return { ...state, allGroups }
+            newState = { ...state };
+            const groups = {};
+            action.groups.forEach(group => groups[group.id] = group);
+            newState.groups = groups;
+            return newState;
 
         default:
             return state
