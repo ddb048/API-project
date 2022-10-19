564b3cff8f3a26e2c6488949aa9f14c9c07d2d31
david <ddb048@gmail.com>
2022-10-19 08:38:31 -0700

    added comments and reworked groups index.js

--- frontend/src/components/Groups/index.js
+++ frontend/src/components/Groups/index.js
@@ -4,19 +4,50 @@ import { getAllGroups } from "../../store/groups";
 import './index.css'
 import GroupListing from "../GroupListing";
 
+
 function Groups() {
     const dispatch = useDispatch();
-    const groups = useSelector(state => state.groups)
+    const groupsObj = useSelector(state => state.groups.groups)
+    const groups = Object.values(groupsObj);
 
+
+    //on each render, triggers the dispatch to backend to fetch all groups
     useEffect(() => {
         dispatch(getAllGroups())
     }, [dispatch]);
 
-    return (allGroups &&
+
+    //declares the variable to hold all listings
+    let groupsList;
+
+
+    //maps over my array of groups and generates a card for each index
+    if (groups.length > 0) {
+        groupsList = (
+            groups.map(group => (<GroupListing group={group} />))
+        )
+
+
+        //sets a display for if I have no groups within my list
+    } else {
+        <div className="no-groups">
+            <img className="no-groups-pic" src="https://secure.meetupstatic.com/next/images/home/EmptyGroup.svg?w=384" />
+            <div className="no-groups-text">There are currently no groups!</div>
+        </div>
+    }
+
+
+
+
+    return (groups &&
         <div className="main">
-            {Object.values(groups).map(group => (
-                <GroupListing group={group} />
-            ))}
+            <div className="groups-inner-container">
+                <Link to=''>Events</Link>
+                <Link to=''>Groups</Link>
+            </div>
+            <div className="groupList">
+                {groupsList}
+            </div>
         </div>
     )
 }
